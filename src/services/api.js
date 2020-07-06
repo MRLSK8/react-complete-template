eed2da2a52c63c3685d0067b4e27e231db3ce1a9
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-07-06 19:55:17 -0300

    chore: added header autorization

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
+import { getUser } from './auth';
+
 export const baseURL = 'http://localhost:3333';
 
 export const api = axios.create({
   baseURL,
+  headers: {
+    Authorization: `Bearer ${getUser().token}`,
+  },
 });
 
 export const fetcher = (path) =>

c4eb801f833d6360df3d3fc57bc95aad533db6f1
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-07-10 18:37:59 -0300

    fix: global style changed

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,11 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   * {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
   }
   body {
-    margin: 0;
-    padding: 0;
     overflow: hidden;
     background-color: ${(props) => props.theme.background}
   }
